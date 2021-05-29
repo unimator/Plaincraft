5b4e55a78c776f1d484be7c96183ad3ea0c1126e
Marcin Górka <unimatorx@gmail.com>
2021-05-29 21:30:54 +0200

    initial commit

---
+++ Plaincraft/Plaincraft.cpp
@@ -0,0 +1,12 @@
+﻿// Plaincraft.cpp : Defines the entry point for the application.
+//
+
+#include "Plaincraft.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
